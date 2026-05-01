29ef42f2bd67fcfd0f36afc62400888405011268
Technology Universe <technologyuniverse364515@gmail.com>
2026-05-02 02:04:47 +0530

    Updates

--- dist/server/assets/_tanstack-start-manifest_v-BGDytQ3D.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/pawar/OneDrive/Desktop/Mehul/FL/aura-cafe/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/aura-cafe/assets/index-CUEh9Ffb.js"] }, "/": { filePath: "C:/Users/pawar/OneDrive/Desktop/Mehul/FL/aura-cafe/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/aura-cafe/assets/index-BZT8tOTH.js"] } }, clientEntry: "/aura-cafe/assets/index-CUEh9Ffb.js" });
-export {
-  tsrStartManifest
-};
